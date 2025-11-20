61d61feafb7b10827ab3034c33038840b6f46a1c
Rodrigo Campos Cabral <142432355+Rodcamppos@users.noreply.github.com>
2025-11-19 21:13:46 -0300

    Clean up svelte.config.js by removing comments
    Removed commented lines to clean up the configuration file.

--- svelte.config.js
+++ svelte.config.js
@@ -3,15 +3,12 @@ import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // 1. O pré-processamento agora usa o método correto (via plugin)
     preprocess: vitePreprocess(), 
 
     kit: {
-        // Deixamos o adapter aqui, mesmo que não vá ser usado no modo dev.
         adapter: adapter() 
     },
-    
-    // (Omitindo o onwarn para manter o código limpo)
+
 };
 
 export default config;
